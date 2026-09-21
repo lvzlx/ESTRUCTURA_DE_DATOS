8d19ef6daf548926f3e90b2746c5f848e75da924
Luz <luzmariarobles18@gmail.com>
2026-09-21 11:25:35 -0600

    feat: agrega seguimiento de pasadas y contadores

--- ordenamiento_burbuja/main_v2.cpp
+++ ordenamiento_burbuja/main_v2.cpp
@@ -1,38 +1,54 @@
 #include <iostream>
 using namespace std;
 
-// Función para ordenar el arreglo con burbuja
+// Función de ordenamiento burbuja
 void burbuja(int numeros[], int n) {
+    int comparaciones = 0;
+    int intercambios = 0;
+
     for (int i = 0; i < n - 1; i++) {
+
         for (int j = 0; j < n - 1 - i; j++) {
+            comparaciones++;
+
             if (numeros[j] > numeros[j + 1]) {
                 int temporal = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temporal;
+
+                intercambios++;
             }
         }
+
+        // Mostrar el arreglo después de cada pasada
+        cout << "Pasada " << i + 1 << ": ";
+
+        for (int j = 0; j < n; j++) {
+            cout << numeros[j] << " ";
+        }
+
+        cout << endl;
     }
+
+    cout << endl;
+    cout << "Elementos: " << n << endl;
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Intercambios: " << intercambios << endl;
 }
 
 int main() {
-    // Crear el arreglo
-    int numeros[] = {8, 3, 5, 1, 9, 2};
-    int n = 6;
+    int numeros[] = {5, 1, 4, 2, 8, 3, 6};
+    int n = 7;
 
-    // Mostrar el arreglo original
     cout << "Arreglo original: ";
+
     for (int i = 0; i < n; i++) {
         cout << numeros[i] << " ";
     }
 
-    // Ordenar el arreglo
+    cout << endl;
+
     burbuja(numeros, n);
-
-    // Mostrar el arreglo ordenado
-    cout << "\nArreglo ordenado: ";
-    for (int i = 0; i < n; i++) {
-        cout << numeros[i] << " ";
-    }
 
     return 0;
 }
