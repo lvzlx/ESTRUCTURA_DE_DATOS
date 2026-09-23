c8b282a607abcf1d93452b6112958b29e1a94c42
Luz <luzmariarobles18@gmail.com>
2026-09-23 10:51:31 -0600

    último commit

--- ordenamiento_burbuja/insertion_sort.cpp
+++ ordenamiento_burbuja/insertion_sort.cpp
@@ -11,6 +11,52 @@ void mostrarArreglo(int numeros[], int n)
     cout << endl;
 }
 
+void insercion(int numeros[], int n)
+{
+    int comparaciones = 0;
+    int desplazamientos = 0;
+    int inserciones = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        int clave = numeros[i];
+        int j = i - 1;
+
+        cout << "Insertando: " << clave << endl;
+
+        while (j >= 0)
+        {
+            comparaciones++;
+
+            if (numeros[j] > clave)
+            {
+                numeros[j + 1] = numeros[j];
+                desplazamientos++;
+                j--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        numeros[j + 1] = clave;
+        inserciones++;
+
+        mostrarArreglo(numeros, n);
+        cout << endl;
+    }
+
+    cout << "========================================" << endl;
+    cout << "           ESTADISTICAS" << endl;
+    cout << "========================================" << endl;
+    cout << endl;
+
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Desplazamientos: " << desplazamientos << endl;
+    cout << "Inserciones: " << inserciones << endl;
+}
+
 int main()
 {
     int numeros[] = {7, 3, 8, 2, 6, 4, 5};
@@ -18,7 +64,9 @@ int main()
 
     cout << "Arreglo original:" << endl;
     mostrarArreglo(numeros, n);
+    cout << endl;
+
+    insercion(numeros, n);
 
     return 0;
 }
-
